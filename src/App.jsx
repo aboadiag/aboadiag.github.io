2260a024323164e922937cb0bd9623355160ca3c
aboadiag <136507019+aboadiag@users.noreply.github.com>
2025-10-24 17:44:35 -0400

    404.html add

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Home } from "./screens/Home";
 import { Projects } from "./screens/Projects";
 import { Research } from "./screens/Research";
 import { ArtPage } from "./screens/ArtPage";
 
+// Define your routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+    {
+    path: "/research",
+    element: <Research />,
+  },
+  {
+    path: "/projects",
+    element: <Projects />,
+  },
+  {
+    path: "/artpage",
+    element: <ArtPage />,
+  },
+]);
+
 export const App = () => {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/research" element={<Research />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/artpage" element={<ArtPage />} />
-      </Routes>
-    </HashRouter>
-  );
+  return <RouterProvider router={router} />;
 };
