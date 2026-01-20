1aa408cabf2051af0a172fe1ac56cbfdf81dc833
Bena! Bena! Bena! <136507019+aboadiag@users.noreply.github.com>
2026-01-19 19:03:51 -0500

    revert

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -119,8 +119,7 @@ export const Home = () => {
               <span className={styles['text-wrapper-71']}>
                 &amp; their respective communities.
                </span>
-               <br /><br />
-
+               
                {/* --- NEW BIO CONTENT START --- */}
                 <span className={styles['text-wrapper-71']}>
                   I completed my B.S. in Mechanical Engineering at Stanford University. 
@@ -290,18 +289,22 @@ export const Home = () => {
              2. Add 'style' prop to position it absolutely at the bottom.
              3. 'top: 2500px' places it right after your education section ends.
           */}
+          <Footer 
+            style={{ 
+              position: 'absolute', 
+              top: '3200px', 
+              width: '100%' 
+            }} 
+          />
         </main>
       </div> 
       {/* End of main-content-canvas */}
 
       {/* 3. Footer - Now outside the canvas, so it sits at the bottom */}
-   <Footer  />
-            {/* style={  */}
-            {/* //   position: 'absolute', 
-            //   top: '3200px', 
-            //   width: '100%' 
-            //  */}
-         
+      {/* <footer>
+        <Footer />
+      </footer>
+       */}
       
     </div>
   );
