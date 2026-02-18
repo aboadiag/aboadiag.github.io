2614cf6da1460de13f1d3f39cd7706db855f17ad
Bena! Bena! Bena! <136507019+aboadiag@users.noreply.github.com>
2026-02-18 18:23:19 -0500

    remove proposal

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -201,9 +201,9 @@ export const Home = () => {
                   <span className={styles['text-wrapper-88']}>
                     Aaron Steinfeld (Chair), Jean Oh, Patrick Carrington, Cynthia L.
                     Bennett
-                    <br />
+                    {/* <br />
                     “Designing Supportive Agents with the Lived Experience of People
-                    with Disabilities” [Proposed Thesis]
+                    with Disabilities” [Proposed Thesis] */}
                   </span>
                 </p>
 
