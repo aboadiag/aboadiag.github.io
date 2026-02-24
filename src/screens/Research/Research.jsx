d37a67c23552f904e35aaa9f98d353db703eb4b9
Bena! Bena! Bena! <136507019+aboadiag@users.noreply.github.com>
2026-02-24 14:54:58 -0500

    update research

--- src/screens/Research/Research.jsx
+++ src/screens/Research/Research.jsx
@@ -43,13 +43,13 @@ export const Research = () => {
         {/* <span className={styles['text-wrapper-122']}>&nbsp;, </span> */}
         <span className={styles['text-wrapper-124']}>behavioral science </span>
         <span className={styles['text-wrapper-122']}>and </span>
-        <span className={styles['text-wrapper-124']}>experimental methods.</span>
+        <span className={styles['text-wrapper-124']}>experimental methods</span>
         {/* <span className={styles['text-wrapper-122']}>
           in my work.
           {/* <br /> */}
           {/* <br />
         </span> */}
-        <span className={styles['text-wrapper-122']}>{""}
+        <span className={styles['text-wrapper-122']}>.&nbsp;&nbsp;
           My work has been published at several top-tier conferences and journals in ACM &amp; IEEE, including
           ASSETS, UIST, HRI, RO-MAN, and THRI.&nbsp;&nbsp;See some of this research below.
         </span>
